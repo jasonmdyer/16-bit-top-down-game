a7ead250cbdcf8011c787e3a531cc0b9e58bd63c
Jason Dyer <152436919+jasonmdyer@users.noreply.github.com>
2024-05-26 22:05:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ charactersMap.forEach((row, i) => {
           },
           scale: 3,
           animate: true,
-          dialogue: ['...', 'Fuck off faggot!']
+          dialogue: ['Fuck off faggot!']
         })
       )
     }
